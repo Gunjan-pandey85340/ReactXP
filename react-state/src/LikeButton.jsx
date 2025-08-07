fc498534b2802727535427f44ef8a0b59319bdf7
Gunjan-pandey85340 <gunjanpandey8979@gmail.com>
2025-08-07 15:28:36 +0530

    Like Button Component

--- react-state/src/LikeButton.jsx
+++ react-state/src/LikeButton.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 export default function LikeButton() {
-    let clicked = () => {
-        console.log("clicked");
-        
-    }
+    let [isLiked ,setIsLiked] = useState(false);
+    let [clicks ,setClicks] = useState(0);
+
+    let toggleLike = () =>{
+        // console.log("we are going to toggle");
+        setIsLiked(!isLiked);
+        setClicks(clicks+1);
+    };
+
+    let likeStyle = {color:"red"};
+
     return (
         <div>
-            <p onClick={clicked}>
-                <i className="fa-regular fa-heart"></i>
+            <p>Clicks = {clicks}</p>
+            <p onClick={toggleLike}>
+            
+                {isLiked ? (
+                    <i className="fa-solid fa-heart" style={likeStyle}></i>
+                ) : (
+                    <i className="fa-regular fa-heart"></i>
+                )}
             </p>
         </div>
     )
